8f833da0ad4bcd747a08b6ecdaa0a8bf03c08487
Sharon Mwenda <79165122+sha123-m@users.noreply.github.com>
2025-03-31 20:28:14 +0300

    error correction

--- src/index.js
+++ src/index.js
@@ -3,46 +3,26 @@ function displayPoem(response) {
       strings: response.data.answer,
       autoStart: true,
       delay: 1,
+      cursor: "",
     });
-  
-    let submitButton = document.querySelector('#submit-button')
-    submitButton.removeAttribute('disabled');
   }
   
-  function displaySheCodesPoem() {
-    let poem = `
-      SheCodes, a community strong and bright,<br />
-      Women learning to code, taking flight.,<br />
-      With determination and support in sight,,<br />
-      Together we shine, in love for tech we unite.,<br />
-      <strong>SheCodes AI</strong>
-      `;
-  
-    new Typewriter("#poem", {
-      strings: poem,
-      autoStart: true,
-      delay: 1,
-    });
-  
-  }
-  
-  function generate(event) {
+  function generatePoem(event) {
     event.preventDefault();
-    let apiKey = "30f662094dfb505f3tcofbd6404ca0a7";
+  
+    let instructionsInput = document.querySelector("#user-instructions");
+    let apiKey = "2046c535afeb092fo82f1d306d8a2b2t";
     let context =
-      "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning. Do not include markdown around it";
-    let instructionsInput = document.querySelector("#instructions");
-    let prompt = `User instructions: Generate a romantic poem about ${instructionsInput.value}`;
-    let apiUrl = `https://api.shecodes.io/ai/v1/generate?context=${context}&prompt=${prompt}&key=${apiKey}`;
-    let submitButton = document.querySelector('#submit-button')
-    submitButton.setAttribute('disabled', 'disabled');
+      "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+    let prompt = `User instructions: Generate a French poem about ${instructionsInput.value}`;
+    let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   
     let poemElement = document.querySelector("#poem");
-      poemElement.classList.remove("hidden");
-    poemElement.innerHTML = `<div class="waiting">⏳ Generating a poem about ${instructionsInput.value}..</div>`;
-    axios.get(apiUrl).then(displayPoem);
+    poemElement.classList.remove("hidden");
+    poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${instructionsInput.value}</div>`;
+  
+    axios.get(apiURL).then(displayPoem);
   }
   
-  let poemForm = document.querySelector("#poem-generator");
-  poemForm.addEventListener("submit", generate);
-  displaySheCodesPoem();
+  let poemFormElement = document.querySelector("#poem-generator-form");
+  poemFormElement.addEventListener("submit", generatePoem);
